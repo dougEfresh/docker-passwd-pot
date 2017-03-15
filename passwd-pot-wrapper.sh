93eaa6955a8ce7fe3238deb8172e5542614eb158
Doug Chimento <dchimento@gmail.com>
2017-03-14 22:14:52 -0400

    adding passwd-pot-wrapper.sh

---
+++ passwd-pot-wrapper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+/usr/local/go/bin/go get -v  github.com/dougEfresh/passwd-pot
+/usr/local/go/bin/go install github.com/dougEfresh/passwd-pot
+nohup su -s /bin/bash nobody -c "/go/bin/passwd-pot potter $PASSWD_POT_OPTS" > /passwd-pot.log &
