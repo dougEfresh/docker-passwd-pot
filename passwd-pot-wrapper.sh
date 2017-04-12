ed415ade7a16f720ca0b109954c96f94031655a7
Doug Chimento <dchimento@gmail.com>
2017-04-12 18:22:00 -0400

    removing go bin

--- passwd-pot-wrapper.sh
+++ passwd-pot-wrapper.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -x
-/usr/local/go/bin/go get -v  github.com/dougEfresh/passwd-pot
-/usr/local/go/bin/go install github.com/dougEfresh/passwd-pot
 nohup su -s /bin/bash nobody -c "/go/bin/passwd-pot potter $PASSWD_POT_OPTS" > /passwd-pot.log &
-
 nohup su -s /bin/bash nobody -c "/go/bin/passwd-pot socket $PASSWD_POT_SOCKET_OPTS" > /passwd-pot-socket.log &
