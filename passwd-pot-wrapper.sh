5b41062cb2369e22c90eefc578c1e0676287794b
Doug Chimento <dchimento@gmail.com>
2018-07-29 21:51:05 +0300

    redirect stderr

--- passwd-pot-wrapper.sh
+++ passwd-pot-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-nohup su -s /bin/bash nobody -c "/bin/passwd-pot potter $PASSWD_POT_OPTS" > /var/log/passwd-pot/pot.log &
-nohup su -s /bin/bash nobody -c "/bin/passwd-pot proxy $PASSWD_POT_PROXY_OPTS" > /var/log/passwd-pot/proxy.log &
+nohup su -s /bin/bash nobody -c "/bin/passwd-pot potter $PASSWD_POT_OPTS" > /var/log/passwd-pot/pot.log 2>&1 &
+nohup su -s /bin/bash nobody -c "/bin/passwd-pot proxy $PASSWD_POT_PROXY_OPTS" > /var/log/passwd-pot/proxy.log 2>&1 &
